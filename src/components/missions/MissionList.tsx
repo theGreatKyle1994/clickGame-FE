ac9779ab643f2e7d12b68a47b52e87a7350a3810
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-05-02 21:05:54 -0400

    fix various typos

--- src/components/missions/MissionList.tsx
+++ src/components/missions/MissionList.tsx
@@ -1,20 +1,20 @@
-import "./MissionList.css";
-import { default as missionsIn } from "../../data/missions.json";
-import { createID } from "../../util/utilityFunctions";
-import { useState } from "react";
-import MissionTile from "./MissionTile";
+// import "./MissionList.css";
+// import { default as missionsIn } from "../../data/missions.json";
+// import { createID } from "../../util/utilityFunctions";
+// import { useState } from "react";
+// import MissionTile from "./MissionTile";
 
-const MissionList = () => {
-  const [missions] = useState<MissionData[]>(missionsIn);
+// const MissionList = () => {
+//   const [missions] = useState<MissionData[]>(missionsIn);
 
-  return (
-    <ul>
-      {missions &&
-        missions.map((mission) => (
-          <MissionTile data={mission} key={createID(mission.gameID)} />
-        ))}
-    </ul>
-  );
-};
+//   return (
+//     <ul>
+//       {missions &&
+//         missions.map((mission) => (
+//           <MissionTile data={mission} key={createID(mission.gameID)} />
+//         ))}
+//     </ul>
+//   );
+// };
 
-export default MissionList;
+// export default MissionList;
